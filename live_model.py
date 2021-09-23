99378268722f4989d5879ece8afb385b2ef957e4
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2021-09-23 14:42:39 -0400

    Updated and working live model trader

--- live_model.py
+++ live_model.py
@@ -16,8 +16,9 @@ from matplotlib import pyplot as plt
 import shutil
 import matplotlib.pyplot as plt
 from dateutil import parser
-from enviroment import Enviroment
 import websocket, json
+import time
+from threading import Thread
 
 
 class LiveModel():
@@ -50,6 +51,8 @@ class LiveModel():
     iteration: list = []
     action_list: list = [] 
 
+    STOP_SIG: bool = False
+
 
     # Model should only care about one trade at a time
     # We can just have 10 separate models going to help negate risk
@@ -58,6 +61,10 @@ class LiveModel():
     # Can easily train this 
 
     # Or maybe just do multiple models and choose the one that resulted in the most profit, kind of like evolution
+
+    # DONT FORGET TO MAKE EVERYTHING RELATIVE
+    # Fuck all that, give the input the array of buy prices, 0's if none exist, then the  model can output an array of 1's and 0's at each index position to dictate whether to sell or not
+    # Fixing the state change in training, just updating prices and shit, the price wouldn't move, should allow the model to fiure out that it can't sell something that doesnt exist
     def __init__(self):
         
         self.api = tradeapi.REST()
@@ -66,7 +73,7 @@ class LiveModel():
             "data": {"key_id": os.environ.get("APCA_API_KEY_ID", None), "secret_key": os.environ.get("APCA_API_SECRET_KEY", None)}
         }
         self.socket_url = "wss://data.alpaca.markets/stream"
-        self.ws = websocket.WebSocketApp(self.socket_url , on_open=self.run, on_message=self.get_minute_data, on_close=self.close)
+        self.ws = websocket.WebSocketApp(self.socket_url , on_open=self.run, on_message=self.update_prices, on_close=self.close)
         
         self.prev_10_BND = deque(maxlen=10)
         self.prev_10_SPY = deque(maxlen=10)
@@ -89,42 +96,64 @@ class LiveModel():
 
         print(f"Closed connection")
 
-    def get_minute_data(self, ws: websocket, message):
+    def  start_action_loop(self):
+
+        while(not self.STOP_SIG):
+
+            if (len(self.prev_10_VTI) == 10 and len(self.prev_10_BND) == 10
+                and len(self.prev_10_SPY) == 10 and len(self.prev_10_VXUS) == 10):
+                print(" all are full, executing decision")
+                self.make_decision()
+            
+            else:
+                print("     Not full yet")
+                print(f"    SPY: {len(self.prev_10_SPY)}")
+                print(f"    VTI: {len(self.prev_10_VTI)}")
+                print(f"    VXUS: {len(self.prev_10_VXUS)}")
+                print(f"    BND: {len(self.prev_10_BND)}")
+            
+            time.sleep(60)
+
+    def update_prices(self, ws: websocket, message: dict):
         
+        message = eval(message)             # Security risk - Fix later
         print(message)
 
-        # Parse the data
-
-        # handle entries with no price
-        curr_SPY_price = 0 # replace
-        curr_VTI_price = 0 # replace
-        curr_VXUS_price = 0 # replace
-        curr_BND_price = 0 # replace
-
-        if not curr_SPY_price:
-            self.prev_10_SPY.append(self.prev_10_SPY[len(self.prev_10_SPY) - 1])
-        else:
-            self.prev_10_SPY.append(curr_SPY_price)
-       
-        if not curr_VTI_price:
-            self.prev_10_VTI.append(self.prev_10_VTI[len(self.prev_10_VTI) - 1])
-        else:
+        if message["stream"] == "AM.VTI":
+            curr_VTI_price = message["data"]["c"]
             self.prev_10_VTI.append(curr_VTI_price)
+            
         
-        if not curr_VXUS_price:
-            self.prev_10_VXUS.append(self.prev_10_VXUS[len(self.prev_10_VXUS) - 1])
-        else:
+        elif message["stream"] == "AM.VXUS":
+            curr_VXUS_price = message["data"]["c"]
             self.prev_10_VXUS.append(curr_VXUS_price)
-       
-        if not curr_BND_price:
-            self.prev_10_BND.append(self.prev_10_BND[len(self.prev_10_BND) - 1])
-        else:
-            self.prev_10_BND.append(curr_BND_price)
+            
         
-        if len(self.prev_10_SPY) == 10:
-            self.make_decision()
+        elif message["stream"] == "AM.BND":
+            curr_BND_price = message["data"]["c"]
+            self.prev_10_BND.append(curr_BND_price)
+            
+        
+        elif message["stream"] == "AM.SPY":
+            curr_SPY_price = message["data"]["c"]
+            self.prev_10_SPY.append(curr_SPY_price)
 
+            if len(self.prev_10_SPY) == 10:
+                self.fill_remaining()
+    
+    def fill_remaining(self):
 
+        if len(self.prev_10_VXUS) > 0:
+            for i in range(10 - len(self.prev_10_VXUS)):
+                self.prev_10_VXUS.append(self.prev_10_VXUS[len(self.prev_10_VXUS) - 1])
+        
+        if len(self.prev_10_BND) > 0:
+            for i in range(10 - len(self.prev_10_BND)):
+                self.prev_10_BND.append(self.prev_10_BND[len(self.prev_10_BND) - 1])
+        
+        if len(self.prev_10_VTI) > 0:
+            for i in range(10 - len(self.prev_10_VTI)):
+                self.prev_10_VTI.append(self.prev_10_VTI[len(self.prev_10_VTI) - 1])
 
     def get_random_action(self):
         return random.randint(1,3)
@@ -136,6 +165,8 @@ class LiveModel():
 
     def execute_buy(self):
         #Implement
+        self.api.submit_order(symbol="SPY", notional=100.00)
+        self.buy_prices.append(self.prev_10_SPY[9])     # Need to make dict and add in id?
         return
 
     def train(self, replay_memory, model, target_model, done):
@@ -169,15 +200,16 @@ class LiveModel():
             Y.append(current_qs)            #
         model.fit(np.array(X), np.array(Y), batch_size=batch_size, verbose=0, shuffle=True)             # Fitting the model to the new input
 
-
     def execute_sell(self):
 
-        #Sell
-        # Update current money
-        # Return current money
-        return
+        self.api.close_position("SPY")
+        equity = self.api.get_account().equity
+        self.curr_money = equity
+
+        return equity
 
 
+    # WE SHOULD INCLUDE EQUITY AS MODEL INPUT
     def execute_action(self, action: int):
   
         reward = 0
@@ -206,10 +238,11 @@ class LiveModel():
     def make_decision(self):
 
         curr_state = self.prev_10_SPY + self.prev_10_VTI + self.prev_10_VXUS + self.prev_10_BND
-        curr_state += len(self.buy_prices)
-        curr_state += self.curr_money
-        curr_state += self.mins_into_week
-        curr_state += self.goal_money
+        curr_state = np.copy(curr_state)
+        curr_state = np.append(curr_state, len(self.buy_prices))
+        curr_state = np.append(curr_state, self.curr_money)
+        curr_state = np.append(curr_state, self.mins_into_week)
+        curr_state = np.append(curr_state, self.goal_money)
 
         self.mins_into_week += 1
 
@@ -251,6 +284,9 @@ class LiveModel():
         ws.send(json.dumps(self.auth_data))
         listen_message = {"action": "listen", "data": {"streams": ["AM.SPY", "AM.VXUS", "AM.VTI", "AM.BND"]}}
         ws.send(json.dumps(listen_message))
+
+        thread = Thread(target = self.start_action_loop, args = ())
+        thread.start()
 
         
 
